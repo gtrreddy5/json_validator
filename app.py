e562b87d4ce9b83ceb8e56daf256bb74a97897ae
Thulasiram <56713204+gtrreddy5@users.noreply.github.com>
2025-12-21 16:05:40 +0530

    Configure Flask app to use environment port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,5 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
